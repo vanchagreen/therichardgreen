73eede373ed6aea3897ee64d483e51cc4429dc1e
vanchagreen <vanchagreen@gmail.com>
2018-01-31 20:00:16 -0700

    Technically a website now!

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static('public'))
 
 app.listen(8080, () => console.log('Server running on port 8080'));
