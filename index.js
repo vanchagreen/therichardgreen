47914b27ea7b24a7d33b194485c23f75276531fd
vanchagreen <vanchagreen@gmail.com>
2018-04-07 21:00:15 -0600

    Moved to landingPage directory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public/landingPage'))
 
 app.listen(8080, () => console.log('Server running on port 8080'));
