159b3b1af9bb79e714aaeaf7de7ed26f22513331
Richard Green <vanchagreen@gmail.com>
2018-06-17 11:23:40 -0600

    Father's Day

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public/landingPage'))
+app.get('/dad', (req, res) => {
+  res.sendFile(__dirname + "/" + "dad.html")
+})
 
 app.listen(8080, () => console.log('Server running on port 8080'));
